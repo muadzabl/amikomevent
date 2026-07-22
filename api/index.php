decb911bb3aa8381dd4e2480a4293f5a65f83538
muadzabl <ablmuadz78@students.amikom.ac.id>
2026-07-23 05:49:36 +0700

    Fix Vercel storage permissions

--- api/index.php
+++ api/index.php
@@ -1,3 +1,23 @@
 <?php
 
+// Paksa direktori storage & bootstrap cache ke /tmp (direktori yang punya izin write di Vercel)
+$_ENV['APP_STORAGE'] = '/tmp/storage';
+
+$storageDirs = [
+    '/tmp/storage',
+    '/tmp/storage/app',
+    '/tmp/storage/framework',
+    '/tmp/storage/framework/cache',
+    '/tmp/storage/framework/sessions',
+    '/tmp/storage/framework/views',
+    '/tmp/storage/bootstrap/cache',
+    '/tmp/storage/logs',
+];
+
+foreach ($storageDirs as $dir) {
+    if (!is_dir($dir)) {
+        mkdir($dir, 0755, true);
+    }
+}
+
 require __DIR__ . '/../public/index.php';
